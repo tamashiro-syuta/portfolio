29c34067946e678eac276d78096f8d6f9c25364d
tamashiro_syuta <syuta5825@gmail.com>
2021-03-13 16:20:33 +0900

    activestrageの実装（画像が表示されない）

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,24 +17,27 @@ class PostsController < ApplicationController
       content: params[:content],
       address: "address",
       latitude: "latitude",
-      longitude: "longitude"
+      longitude: "longitude",
+      images: []
       # 住所も追加する（address,latitude,longitude）
     )
     
-      @images = params[:post_image]
-      # Imagesテーブルに画像のパスを入れて、post_imageテーブルにさっきImagesに入れたデータのidを入れる。（postのidと一緒に入れる）
-      # これを画像の枚数分繰り返す
-      @images.each do |image|
-        tempfile = image.tempfile
-        @save_to = "public/post_images/#{image.filename}"
-        FIleUtils.mv(tempfile, save_to)
-        Image.new(
-          save_to: @save_to
-        )
-      end
+      # @images = params[:post_image]
+      # # Imagesテーブルに画像のパスを入れて、post_imageテーブルにさっきImagesに入れたデータのidを入れる。（postのidと一緒に入れる）
+      # # これを画像の枚数分繰り返す
+      # @images.each do |image|
+      #   tempfile = image.tempfile
+      #   @save_to = "public/post_images/#{image.filename}"
+      #   FIleUtils.mv(tempfile, save_to)
+      #   Image.new(
+      #     save_to: @save_to
+      #   )
+      # end
       
-    if @post.save && @image.save
+    if @post.save
       flash[:notice] = "投稿を作成しました"
+      
+      binding.irb
       redirect_to("/users/#{@current_user.id}") # タイムラインに飛ばす(今は仮に投稿詳細ページに飛ばしている)
     else
       render("home#top") #　マイページに飛ばす
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,14 @@
 class Post < ApplicationRecord
-    has_many :images, dependent: :destroy
+    has_many_attached :images
 
     # @user.post とやるとそのユーザーの投稿を全て取得
     def posts
         return Post.where(user_id: self.id)
     end
+
+    # 画像のリサイズ（使う際には　画像.thumbnail　の形）
+    def thumbnail
+        return self.image.variant(resize: '300x300').processed
+      end      
 
 end
